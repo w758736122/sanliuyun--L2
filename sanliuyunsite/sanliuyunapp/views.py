99a94a940644b5470e3a8f76632447877fd092e6
w758736122 <jiang_jy@outlook.com>
2016-11-06 21:32:27 +0800

    完善用户登陆注册页面细节
    完善用户登陆注册页面细节，修复了2个BUG

--- sanliuyunsite/sanliuyunapp/views.py
+++ sanliuyunsite/sanliuyunapp/views.py
@@ -3,17 +3,14 @@ from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth import authenticate,login
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from sanliuyunapp.form import registerForm,loginForm
-from sanliuyunapp.models import Person
+from sanliuyunapp.form import registerForm,loginForm, ArticleForm
+from sanliuyunapp.models import Person, Article
+
 
 def indexView(request):
     context = {}
     return render(request,'index.html',context)
 
-
-def desktopView(request):
-    context = {}
-    return render(request,'desktop.html',context)    
 
 
 def loginView(request):
@@ -25,21 +22,22 @@ def loginView(request):
         if form.is_valid():
             inputName = form.cleaned_data['inputName']
             password = form.cleaned_data['password']
-            user = authenticate(username =inputName,password = password)
-            if user:
-                login(request,user)
-                return redirect(to = 'index')
-            else:
-                person = Person.objects.get(email_address = inputName)
-                inputName = person.nickname
+            try:
                 user = authenticate(username =inputName,password = password)
                 if user:
                     login(request,user)
                     return redirect(to = 'index')
                 else:
-                    return HttpResponse('用户名/密码错误')
-
-
+                    person = Person.objects.get(email_address = inputName)
+                    inputName = person.nickname
+                    user = authenticate(username =inputName,password = password)
+                    if user:
+                        login(request,user)
+                        return redirect(to = 'index')
+                    else:
+                        return HttpResponse('用户名/密码错误')
+            except Person.DoesNotExist:
+                return HttpResponse('用户名不存在')
             return redirect(to = 'index')
     context['form']= form
     return render(request,'login.html',context)
@@ -57,11 +55,14 @@ def registerView(request):
             if nickname ==email_address:
                 return HttpResponse('昵称和邮箱地址不能一样哦~')
             try:
+                nickname_judge = Person.objects.get(nickname = nickname )
+                if nickname_judge:
+                    return HttpResponse('用户名重复了~')
+            except:
                 email_judge= Person.objects.get(email_address = email_address )
                 if email_judge:
                     return HttpResponse('邮箱重复了~')
             ##判断是否邮箱已经存在
-            except:
                 password1 = form.cleaned_data['password1']
                 password2 = form.cleaned_data['password2']
                 if password1 == password2:
@@ -73,8 +74,29 @@ def registerView(request):
                     if user:
                         login(request,user)
                         return redirect(to = 'index')
-
                 else:
                     return HttpResponse('2次密码不同，请重新输入')
     context['form']= form
     return render(request,'register.html',context)
+
+
+def editorView(request):
+
+    context = {}
+    if request.method == 'GET':
+        form = ArticleForm
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            headline = form.cleaned_data['headline']
+            content = form.cleaned_data['content']
+            a = Article(headline=headline, text=content)
+            a.save()
+
+
+    # article = Article.objects.get()
+    context['form'] = form
+    # context['article'] = article
+    editor_page = render(request, 'editoring.html', context)
+
+    return editor_page
--- sanliuyunsite/sanliuyunsite/urls.py
+++ sanliuyunsite/sanliuyunsite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from sanliuyunapp.views import registerView,loginView,indexView,desktopView
+from sanliuyunapp.views import registerView,loginView,indexView, editorView
 from django.contrib.auth.views import logout
 
 urlpatterns = [
@@ -25,5 +25,5 @@ urlpatterns = [
     url(r'^register/', registerView,name='register'),
     url(r'^login/', loginView,name='login'),
     url(r'^logout/', logout,{'next_page': '/index'},name='logout'),
-    url(r'^desktop/', desktopView,name='desktop'),
+    url(r'^editor', editorView, name='editor'),
 ]
